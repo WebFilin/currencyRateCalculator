08146f65018452c4fc625071bc691f1120e5c452
WebFilin <filimigr@gmail.com>
2022-07-09 17:13:21 +0500

    Подключенны обработчики событий выбора одной валюты, ввода нужного курса

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
-const rateItemsValue = document.querySelector(".currency-body__wrapper-items");
-
+const rateItemsInput = document.querySelector(".currency-body__wrapper-items");
 const rateItemsRate = document.querySelector(".currency-body__wrapper-rate");
+const curseValueInput = document.querySelector(
+  ".currency-body__currency-curse-input"
+);
+const curseValueRate = document.querySelector(
+  ".currency-body__currency-curse-rate"
+);
 
-//Названия основные валюты
+// Получаем инпут
+const inputCurse = document.querySelector(".currency-body__currency-input");
+
+// Отрисовываем введенный в инпут курс 
+const drowInputCurse = document.querySelector(".currency-body__view-rate");
+
+// ************
+//Основные валюты
 const baseCurrencyName = ["USD", "EUR", "GBP", "CNY", "JPY", "CHF"];
 
 // Массив всех курсов валют
@@ -10,6 +22,21 @@ const arrAllCurrency = [];
 
 // Базовые валюты для первичного отображения
 const arrBaseCurrency = [];
+
+// Деструктурированный массив курсов валют
+const arrRateCurrency = [];
+
+//   Массив кросс курсов валют
+const arrCrossRateCurrency = [];
+// ************
+
+// Глобальные переменные для курсов валют
+let inputValue = null;
+
+// Переменные для кликов
+// По дефолту рубли
+let checkCurrencyInput = null;
+let checkCurrencyCross = null;
 
 // Получаем базу курсов валют
 async function getCurrencyData() {
@@ -31,11 +58,6 @@ async function getCurrencyData() {
 
 // Собираем курсы валют, кросс курсы, коды стран и названия в массив
 function arrCurrency(crossCurrency, rateCurrency) {
-  //   Массив курсов валют
-  const arrRateCurrency = [];
-
-  const arrCrossRateCurrency = [];
-
   //   Деструктурируем обьект значений курсов
   Object.entries(rateCurrency).map(([key, value]) => {
     arrRateCurrency.push({
@@ -50,7 +72,7 @@ function arrCurrency(crossCurrency, rateCurrency) {
   Object.entries(crossCurrency).map(([key, value]) => {
     arrCrossRateCurrency.push({
       countryCode: key,
-      crossRate: value,
+      currencyDay: value,
     });
   });
 
@@ -82,6 +104,7 @@ function filterCurrency() {
 
 // Отрисовываем базовые валюты для приложения
 function drowItemsBaseCurrency() {
+  // Массив с правильным порядком валют
   let arrDrow = [
     arrBaseCurrency[5],
     arrBaseCurrency[4],
@@ -89,13 +112,14 @@ function drowItemsBaseCurrency() {
     arrBaseCurrency[0],
     arrBaseCurrency[2],
     arrBaseCurrency[1],
+    arrCrossRateCurrency[34],
   ];
 
   arrDrow.map((elem) => {
-    rateItemsValue.insertAdjacentHTML(
+    rateItemsInput.insertAdjacentHTML(
       "afterBegin",
       `<button  
-      class="currency-body__items-btn" 
+      class="currency-body__input-btn" 
       value=${elem.currencyDay}>
       ${elem.countryCode}
       </button>
@@ -104,16 +128,61 @@ function drowItemsBaseCurrency() {
 
     rateItemsRate.insertAdjacentHTML(
       "afterBegin",
-      `<div class="currency-body__items">
-      <p>${elem.countryCode}</p>
-      <p> ${elem.currencyDay}</p>
-      </div>`
+      `<button
+      class="currency-body__rate-btn" 
+      value=${elem.currencyDay}
+      >
+      ${elem.countryCode}
+      </button> 
+`
     );
   });
+
+  // Отображаемый по дефолту курс при загрузке
+  curseValueInput.innerHTML = `1 ${arrCrossRateCurrency[34].countryCode} = ${arrCrossRateCurrency[34].currencyDay} USD`;
 }
 
-rateItemsValue.addEventListener("click", (event) => {
-  console.log(event.target.value);
+//  Выбираем валюту для ввода
+function handlerClickBtnInput(checkBtn) {
+  const txtValue = checkBtn.target.innerText;
+  const valueRate = checkBtn.target.value;
+
+  checkCurrencyInput = valueRate;
+
+  // Вставляем строчку с курсом в виджет ввода
+  curseValueInput.innerHTML = `1 ${txtValue} = ${valueRate} RUB `;
+}
+
+//    Выбираем валюту для кросскурса
+function handlerClickBtnRate(checkBtn) {
+  const txtValue = checkBtn.target.innerText;
+  const valueRate = checkBtn.target.value;
+
+  // Вставляем строчку с курсом в виджет кросскурса
+  curseValueRate.innerHTML = ` 1 ${txtValue} = ${valueRate} `;
+}
+
+// Обрабтываем ввод инпута, проверям что вводят только числа
+function handlerInput(value) {
+  console.log(arrCrossRateCurrency[34]);
+  if (!isNaN(parseFloat(value)) && isFinite(value)) {
+    inputValue = value;
+
+    if (checkCurrencyInput === null) {
+      checkCurrencyInput = arrCrossRateCurrency[34].currencyDay;
+    }
+
+    console.log(checkCurrencyInput * value);
+  }
+}
+
+rateItemsInput.addEventListener("click", (click) =>
+  handlerClickBtnInput(click)
+);
+rateItemsRate.addEventListener("click", (click) => handlerClickBtnRate(click));
+
+inputCurse.addEventListener("input", (input) => {
+  handlerInput(input.target.value);
 });
 
 getCurrencyData();
